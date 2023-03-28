cbbe964911c25482f9a6dd98c3a06ab551bf0c92
farisnif <fan5061@psu.edu>
2023-03-27 22:12:04 -0400

    js working using shadoroot and firstUpdated method

--- src/badge-edu.js
+++ src/badge-edu.js
@@ -1,88 +1,126 @@
-import { LitElement, html, css } from 'lit';
+import {LitElement, html, css} from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
-class BadgeEdu extends LitElement {
+export class BadgeEdu extends LitElement {
   static properties = {
-    header: { type: String },
+    version: {},
+  };
+
+  static get styles() {
+    return css`
+          * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+
+    .accordion-container {
+      background: #c00a0a;
+      width: 400px;
+      border: 1px solid #ccc;
+      margin-top: 80px;
+      padding: 20px;
+    }
+    .accord-header {
+      background: black;
+      color: #fff;
+      padding: 1rem 1.25rem;
+      cursor: pointer;
+    }
+    .accord-content {
+      height: 120px;
+      overflow: hidden;
+      transition: all 0.35s ease-out;
+      transform: scaleY(1);
+      transform-origin: top;
+    }
+    .accord-body {
+      padding: 1rem 1.25rem;
+    }
+    .accord-item {
+      border-bottom: 1px solid #ccc;
+    }
+    .accord-item:last-of-type {
+      border: none;
+    }
+    .collapse {
+      height: 0px;
+      transform: scaleY(0);
+    }
+      `;
   }
-
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--badge-edu-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.version = 'STARTING';
+  }
+
+  firstUpdated() {
+    const accrodHeaders = this.shadowRoot.querySelectorAll(".accord-header");
+    accrodHeaders.forEach((ach) => {
+      ach.addEventListener("click", toggelItem, false);
+    });
+
+    function toggelItem() {
+      const currentContentEle = this.nextElementSibling;
+
+      const isCollapsed = currentContentEle.classList.contains("collapse");
+
+      accrodHeaders.forEach((ach) => {
+        const contentEle = ach.nextElementSibling;
+        if (!contentEle.classList.contains("collapse")) {
+          contentEle.classList.add("collapse");
+        }
+      });
+
+      if (isCollapsed) {
+        currentContentEle.classList.remove("collapse");
+      }
+    }
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/BadgeEdu.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+     <h1>Badge (5)</h1>
+      <div class="accord-item">
+        <div class="accord-header">
+          <div class="acccord-title">Accordion Item #1</div>
+        </div>
+        <div class="accord-content collapse">
+          <div class="accord-body">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus,
+            dolor neque! Accusamus mollitia, cumque harum iste nesciunt unde
+            natus incidunt eius non explicabo sed vel voluptas facilis ad minus
+            consequuntur!
+          </div>
+        </div>
+      </div>
+      <div class="accord-item">
+        <div class="accord-header">
+          <div class="acccord-title">Accordion Item #2</div>
+        </div>
+        <div class="accord-content collapse">
+          <div class="accord-body">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus,
+            dolor neque! Accusamus mollitia, cumque harum iste nesciunt unde
+            natus incidunt eius non explicabo sed vel voluptas facilis ad minus
+            consequuntur!
+          </div>
+        </div>
+      </div>
+      <div class="accord-item">
+        <div class="accord-header">
+          <div class="acccord-title">Accordion Item #3</div>
+        </div>
+        <div class="accord-content collapse">
+          <div class="accord-body">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus,
+            dolor neque! Accusamus mollitia, cumque harum iste nesciunt unde
+            natus incidunt eius non explicabo sed vel voluptas facilis ad minus
+            consequuntur!
+          </div>
+        </div>
+      </div>
     `;
   }
 }
-
 customElements.define('badge-edu', BadgeEdu);
