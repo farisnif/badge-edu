bb759cdd50f87c8ad85c09c692df26a4f572e414
farisnif <fan5061@psu.edu>
2023-04-04 11:36:43 -0400

    added page header above srch bar-needs font chnge

--- src/badge-roster.js
+++ src/badge-roster.js
@@ -16,6 +16,8 @@ export class BadgeRoster extends LitElement {
         this.badges = [];
         this.wiki = 'Badges (5)';
         this.updateRoster();
+        this.welcomeText = 'Here you will learn how to set up an account for AWS, how to create a root user, a I AM user account, setup MFA on your Root and I AM accounts, set up a user pool in Amazon Cognito, how to upload and use Amazon S3, and how to use Amazon Lambda';
+        this.pageTitle = 'Explore';
     }
 
     updateRoster() {
@@ -43,16 +45,28 @@ export class BadgeRoster extends LitElement {
         .item {
             display: inline;
         }
+        .welcomeText {
+            background-color: white;
+            padding: 20px;
+            font-size: 20px;
+            /* need to add font */
+        }
+        .pageTitle {
+            padding: 20px;
+            font-size: 32px;
+        }
         `;
     }
 
     render() {
         return html`
-        <form style="display: flex; justify-content: center; margin-top: 20px;">
+        <div class="pageTitle">${this.pageTitle}</div>
+      <div class="welcomeText">${this.welcomeText}
+      <form style="display: flex; justify-content: center; margin-top: 20px;">
         <input type="text" placeholder="Search Content, Topics, and People" id="search-input" style="padding: 10px; width: 300px; border-radius: 4px; border: none; box-shadow: 0 2px 4px rgba(0,0,0,0.2);">
         <button type="submit" style="padding: 10px 20px; margin-left: 10px; background-color: #007bff; color: #fff; border-radius: 4px; border: none; cursor: pointer;">Search</button>
       </form>
-      
+    </div>
             <h2>${this.wiki}</h2>
             ${this.badges.map(badge => html`
             <div class="item">
